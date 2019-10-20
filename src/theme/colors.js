69f1c34843bd3e46ce9bf66fb62f6d8d631a3121
Luís Felipe Souza <luisfmsouza@gmail.com>
2019-10-20 19:51:24 -0300

    Create theme colors constants
    User colors from Shopify Polaris:
    https://polaris.shopify.com/design/colors#navigation

---
+++ src/theme/colors.js
@@ -0,0 +1,8 @@
+export const skyLighter = "#F9FAFB";
+export const skyLight = "#F4F6F8";
+export const sky = "#DFE3E8";
+export const skyDark = "#C4CDD5";
+export const inkLightest = "#919EAB";
+export const inkLighter = "#637381";
+export const inkLight = "#454F5B";
+export const ink = "#212B36";
